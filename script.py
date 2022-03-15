bcb853f415ac56504b0f2f190c0932b4757b4bc3
Samuel Grayson <sam@samgrayson.me>
2022-03-15 02:19:09 -0500

    Fix script

--- script.py
+++ script.py
@@ -337,7 +337,12 @@ def dct_to_args(dct: Mapping[str, Union[bool, int, float, str]]) -> List[str]:
 
 
 @app.command()
-def publish(version_part: VersionPart, verify: bool = True, bump: bool = True) -> None:
+def publish(
+        version_part: VersionPart,
+        verify: bool = True,
+        docs: bool = True,
+        bump: bool = True,
+) -> None:
     if verify:
         asyncio.run(all_tests_inner(True))
     elif docs:
